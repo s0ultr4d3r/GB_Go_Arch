6cf74cacfb2019c74240f5651fd8036d570dd526
Anton Bondar <s0ul.tr4d3r@gmail.com>
2021-02-27 09:40:52 +0300

    Lesson5
    code formatting

--- lesson-5/doublelinkedlist/doublelinkedlist.go
+++ lesson-5/doublelinkedlist/doublelinkedlist.go
@@ -1,33 +1,13 @@
-// Copyright 2009 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Package list implements a doubly linked list.
-//
-// To iterate over a list (where l is a *List):
-//	for e := l.Front(); e != nil; e = e.Next() {
-//		// do something with e.Value
-//	}
-//
 package doublelinkedlist
 
-// Elem is an element of a linked list.
 type Elem struct {
-	// Next and previous pointers in the doubly-linked list of elements.
-	// To simplify the implementation, internally a list l is implemented
-	// as a ring, such that &l.keyElem is both the next element of the last
-	// list element (l.Back()) and the previous element of the first list
-	// element (l.Front()).
 	next, prev *Elem
 
-	// The list to which this element belongs.
 	list *List
 
-	// The value stored with this element.
 	Value interface{}
 }
 
-// Next returns the next list element or nil.
 func (e *Elem) Next() *Elem {
 	if p := e.next; e.list != nil && p != &e.list.keyElem {
 		return p
@@ -35,7 +15,6 @@ func (e *Elem) Next() *Elem {
 	return nil
 }
 
-// Prev returns the previous list element or nil.
 func (e *Elem) Prev() *Elem {
 	if p := e.prev; e.list != nil && p != &e.list.keyElem {
 		return p
@@ -43,14 +22,11 @@ func (e *Elem) Prev() *Elem {
 	return nil
 }
 
-// List represents a doubly linked list.
-// The zero value for List is an empty list ready to use.
 type List struct {
-	keyElem Elem // sentinel list element, only &keyElem, keyElem.prev, and keyElem.next are used
-	len     int  // current list length excluding (this) sentinel element
+	keyElem Elem
+	len     int
 }
 
-// Init initializes or clears list l.
 func (l *List) Init() *List {
 	l.keyElem.next = &l.keyElem
 	l.keyElem.prev = &l.keyElem
@@ -58,14 +34,10 @@ func (l *List) Init() *List {
 	return l
 }
 
-// New returns an initialized list.
 func New() *List { return new(List).Init() }
 
-// Len returns the number of elements of list l.
-// The complexity is O(1).
 func (l *List) Len() int { return l.len }
 
-// Front returns the first element of list l or nil if the list is empty.
 func (l *List) Front() *Elem {
 	if l.len == 0 {
 		return nil
@@ -73,7 +45,6 @@ func (l *List) Front() *Elem {
 	return l.keyElem.next
 }
 
-// Back returns the last element of list l or nil if the list is empty.
 func (l *List) Back() *Elem {
 	if l.len == 0 {
 		return nil
@@ -81,14 +52,12 @@ func (l *List) Back() *Elem {
 	return l.keyElem.prev
 }
 
-// zeroList lazily initializes a zero List value.
 func (l *List) zeroList() {
 	if l.keyElem.next == nil {
 		l.Init()
 	}
 }
 
-// insert inserts e after at, increments l.len, and returns e.
 func (l *List) insert(e, at *Elem) *Elem {
 	e.prev = at
 	e.next = at.next
@@ -99,23 +68,20 @@ func (l *List) insert(e, at *Elem) *Elem {
 	return e
 }
 
-// insertValue is a convenience wrapper for insert(&Elem{Value: v}, at).
 func (l *List) insertValue(v interface{}, at *Elem) *Elem {
 	return l.insert(&Elem{Value: v}, at)
 }
 
-// remove removes e from its list, decrements l.len, and returns e.
 func (l *List) remove(e *Elem) *Elem {
 	e.prev.next = e.next
 	e.next.prev = e.prev
-	e.next = nil // avoid memory leaks
-	e.prev = nil // avoid memory leaks
+	e.next = nil
+	e.prev = nil
 	e.list = nil
 	l.len--
 	return e
 }
 
-// move moves e to next to at and returns e.
 func (l *List) move(e, at *Elem) *Elem {
 	if e == at {
 		return e
@@ -131,77 +97,56 @@ func (l *List) move(e, at *Elem) *Elem {
 	return e
 }
 
-// Remove removes e from l if e is an element of list l.
-// It returns the element value e.Value.
-// The element must not be nil.
 func (l *List) Remove(e *Elem) interface{} {
 	if e.list == l {
-		// if e.list == l, l must have been initialized when e was inserted
-		// in l or l == nil (e is a zero Elem) and l.remove will crash
+
 		l.remove(e)
 	}
 	return e.Value
 }
 
-// PushFront inserts a new element e with value v at the front of list l and returns e.
 func (l *List) PushFront(v interface{}) *Elem {
 	l.zeroList()
 	return l.insertValue(v, &l.keyElem)
 }
 
-// PushBack inserts a new element e with value v at the back of list l and returns e.
 func (l *List) PushBack(v interface{}) *Elem {
 	l.zeroList()
 	return l.insertValue(v, l.keyElem.prev)
 }
 
-// InsertBefore inserts a new element e with value v immediately before mark and returns e.
-// If mark is not an element of l, the list is not modified.
-// The mark must not be nil.
 func (l *List) InsertBefore(v interface{}, mark *Elem) *Elem {
 	if mark.list != l {
 		return nil
 	}
-	// see comment in List.Remove about initialization of l
+
 	return l.insertValue(v, mark.prev)
 }
 
-// InsertAfter inserts a new element e with value v immediately after mark and returns e.
-// If mark is not an element of l, the list is not modified.
-// The mark must not be nil.
 func (l *List) InsertAfter(v interface{}, mark *Elem) *Elem {
 	if mark.list != l {
 		return nil
 	}
-	// see comment in List.Remove about initialization of l
+
 	return l.insertValue(v, mark)
 }
 
-// MoveToFront moves element e to the front of list l.
-// If e is not an element of l, the list is not modified.
-// The element must not be nil.
 func (l *List) MoveToFront(e *Elem) {
 	if e.list != l || l.keyElem.next == e {
 		return
 	}
-	// see comment in List.Remove about initialization of l
+
 	l.move(e, &l.keyElem)
 }
 
-// MoveToBack moves element e to the back of list l.
-// If e is not an element of l, the list is not modified.
-// The element must not be nil.
 func (l *List) MoveToBack(e *Elem) {
 	if e.list != l || l.keyElem.prev == e {
 		return
 	}
-	// see comment in List.Remove about initialization of l
+
 	l.move(e, l.keyElem.prev)
 }
 
-// MoveBefore moves element e to its new position before mark.
-// If e or mark is not an element of l, or e == mark, the list is not modified.
-// The element and mark must not be nil.
 func (l *List) MoveBefore(e, mark *Elem) {
 	if e.list != l || e == mark || mark.list != l {
 		return
@@ -209,9 +154,6 @@ func (l *List) MoveBefore(e, mark *Elem) {
 	l.move(e, mark.prev)
 }
 
-// MoveAfter moves element e to its new position after mark.
-// If e or mark is not an element of l, or e == mark, the list is not modified.
-// The element and mark must not be nil.
 func (l *List) MoveAfter(e, mark *Elem) {
 	if e.list != l || e == mark || mark.list != l {
 		return
@@ -219,8 +161,6 @@ func (l *List) MoveAfter(e, mark *Elem) {
 	l.move(e, mark)
 }
 
-// PushBackList inserts a copy of another list at the back of list l.
-// The lists l and other may be the same. They must not be nil.
 func (l *List) PushBackList(other *List) {
 	l.zeroList()
 	for i, e := other.Len(), other.Front(); i > 0; i, e = i-1, e.Next() {
@@ -228,8 +168,6 @@ func (l *List) PushBackList(other *List) {
 	}
 }
 
-// PushFrontList inserts a copy of another list at the front of list l.
-// The lists l and other may be the same. They must not be nil.
 func (l *List) PushFrontList(other *List) {
 	l.zeroList()
 	for i, e := other.Len(), other.Back(); i > 0; i, e = i-1, e.Prev() {
